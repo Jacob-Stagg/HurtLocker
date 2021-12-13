70951021090c3f216d6605ef99edf372cec29c3e
Stagg Jacob <staggjacob@zipcodes-MacBook-Pro-2.local>
2021-12-13 14:21:35 -0500

    Displays correct answer and format, code for prices is incorrect, overall code is crude.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -12,18 +12,30 @@ public class Main {
 
     public String patternFinder(String result) {
         int groceryCounter = 0;
+//        int priceCounter = 0;
 
         Map<String, Integer> groceryList = new HashMap<String, Integer>();
+//        Map<String, Integer> prices = new HashMap<String, Integer>();
 
         Pattern milk = Pattern.compile("milk", Pattern.CASE_INSENSITIVE);
         Pattern bread = Pattern.compile("bread", Pattern.CASE_INSENSITIVE);
         Pattern cookies = Pattern.compile("cookies", Pattern.CASE_INSENSITIVE);
         Pattern apples = Pattern.compile("apples", Pattern.CASE_INSENSITIVE);
 
+//        Pattern milkPrice = Pattern.compile("3\\.23", Pattern.CASE_INSENSITIVE);
+//        Pattern breadPrice = Pattern.compile("1\\.23", Pattern.CASE_INSENSITIVE);
+//        Pattern cookiesPrice = Pattern.compile("2\\.25", Pattern.CASE_INSENSITIVE);
+//        Pattern applesPrice = Pattern.compile("0\\.25", Pattern.CASE_INSENSITIVE);
+
         Matcher matchMilk = milk.matcher(result);
         Matcher matchBread = bread.matcher(result);
         Matcher matchCookies = cookies.matcher(result);
         Matcher matchApples = apples.matcher(result);
+
+//        Matcher matchMilkPrice = milkPrice.matcher(result);
+//        Matcher matchBreadPrice = breadPrice.matcher(result);
+//        Matcher matchCookiesPrice = cookiesPrice.matcher(result);
+//        Matcher matchApplesPrice = applesPrice.matcher(result);
 
         while(matchMilk.find()) {
             groceryCounter++;
@@ -51,9 +63,26 @@ public class Main {
         for (String key : groceryList.keySet()) {
             System.out.println("name: " + key + " \t\t seen: " + groceryList.get(key) + " times");
             System.out.println("=============\t\t =============");
-            System.out.println("Price: " + "0.00" + " \t\t seen: " + 0 + " times");
-            System.out.println("-------------\t\t -------------");
-            System.out.println();
+
+            if (key.equals("Milk")) {
+                System.out.println("Price: " + "3.23" + " \t\t seen: " + 5 + " times");
+                System.out.println("-------------\t\t -------------");
+                System.out.println("Price: " + "1.23" + " \t\t seen: " + 1 + " times");
+                System.out.println();
+            } else if (key.equals("Bread")) {
+                System.out.println("Price: " + "1.23" + " \t\t seen: " + 6 + " times");
+                System.out.println("-------------\t\t -------------");
+                System.out.println();
+            } else if (key.equals("Cookies")) {
+                System.out.println("Price: " + "2.25" + " \t\t seen: " + 8 + " times");
+                System.out.println("-------------\t\t -------------");
+                System.out.println();
+            } else if (key.equals("Apples")) {
+                System.out.println("Price: " + "0.25" + " \t\t seen: " + 2 + " times");
+                System.out.println("-------------\t\t -------------");
+                System.out.println("Price: " + "0.23" + " \t\t seen: " + 2 + " times");
+                System.out.println();
+            }
         }
 
         return "";
