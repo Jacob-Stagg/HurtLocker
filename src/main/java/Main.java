0121035a010249edbab3e7535f1e79337ad6a5c6
Stagg Jacob <staggjacob@zipcodes-MacBook-Pro-2.local>
2021-12-13 10:29:29 -0500

    Added in patternFinder class, work in progress.

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,19 +1,32 @@
 import org.apache.commons.io.IOUtils;
 import java.io.IOException;
-import java.util.regex.Matcher;
-import java.util.regex.Pattern;
+import java.util.regex.*;
 
 public class Main {
 
-    public String readRawDataToString() throws Exception{
+    public String readRawDataToString() throws Exception {
         ClassLoader classLoader = getClass().getClassLoader();
         String result = IOUtils.toString(classLoader.getResourceAsStream("RawData.txt"));
         return result;
     }
 
-    public static void main(String[] args) throws Exception{
-        String output = (new Main()).readRawDataToString();
-        System.out.println(output);
+    public String patternFinder(String result) throws Exception {
+        String grocerylist = "complete";
+        int counter = 0;
+        Pattern pattern = Pattern.compile("milk", Pattern.CASE_INSENSITIVE);
+        Matcher match = pattern.matcher(result);
 
+        while(match.find()) {
+            counter++;
+            System.out.println("Match no:" + counter);
+            System.out.println("Found at: " + match.start() + " - " + match.end());
+        }
+
+        return grocerylist;
+    }
+
+    public static void main(String[] args) throws Exception {
+        String output = new Main().patternFinder(new Main().readRawDataToString());
+        System.out.println(output);
     }
 }
